0260d9e5478f968e731cd62ca1126881176a7608
Wilson <waramos97@gmail.com>
2020-02-28 01:02:57 -0300

    Aplicativo OK

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:gif_searcher/ui/home_page.dart';
+import 'package:gif_searcher/ui/gif_page.dart';
+void main(){
+  runApp(MaterialApp(
+    home:HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
+
+
+
